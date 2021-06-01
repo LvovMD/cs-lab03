a1cacea1d7b0f66732514061fa2c4d9ace4a3d0a
LvovMD <LvovMD@mpei.ru>
2021-06-01 08:23:44 +0300

    project: Тестовый проект и базовые тесты.

---
+++ test.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <math.h>
+#include <cscobj.h>
+#include <vector>
+#include <Windows.h>
+#include <iterator>
+#include "histogram.h"
+
+#include <cassert>
+
+void test_positive()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 2, 3}, min, max);
+    assert(min == 1);
+    assert(max == 3);
+}
+
+void test_negative()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == -1);
+}
+void test_equal()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 1, 1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+
+void test_one()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+
+void test_null()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+}
+
+int main()
+{
+    test_positive();
+    test_negative();
+    test_equal();
+    test_one();
+}
