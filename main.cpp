39637e5aaf14a6c69491f95da9c2ba102a03961a
LvovMD <LvovMD@mpei.ru>
2021-06-01 08:10:45 +0300

    code: Добавление svg.

--- main.cpp
+++ main.cpp
@@ -96,6 +96,87 @@ void show_histogramm_text(vector<size_t> bins, size_t bin_count, vector<vector<c
     }
 }
 
+void svg_begin(double width, double height)
+{
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_end()
+{
+    cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, string text)
+{
+    cout << "<text x='"<<left<<"' y='"<<baseline<<"'>"<<text<<"</text>";
+}
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
+{
+    cout << "<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
+}
+
+void svg_line(double x1, float y1, double x2, float y2, string stroke, float stroke_width, double stroke_dash, double stroke_dasharray)
+{
+    cout << "<line x1='"<<x1<<"' y1='"<<y1<<"' x2='"<<x2<<"' y2='"<<y2<<"' stroke='"<<stroke<<"' stroke-width='"<<stroke_width<<"' stroke-dasharray = '"<<stroke_dash<<" "<<stroke_dasharray<<"'/>";
+}
+
+void show_histogram_svg(vector<size_t> bins, size_t bin_count, vector<vector<char>> bin_naming_list)
+{
+
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    double BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    vector<char[80]> bin_namings(bin_count);
+    size_t iter = 0;
+    for (size_t iter = 0; iter < bin_count; iter++)
+    {
+        for (char l : bin_naming_list[iter])
+        {
+            bin_namings[iter][strlen(bin_namings[iter])] = l;
+        }
+    }
+    double max_len = strlen(bin_namings[0]);
+    for (size_t i = 0; i < bin_count; i++)
+    {
+        if (strlen(bin_namings[i]) > max_len)
+        {
+            max_len = strlen(bin_namings[i]);
+        }
+    }
+    double max_count = bins[0];
+    for (double x : bins)
+    {
+        if (x > max_count)
+        {
+            max_count = x;
+        }
+    }
+    if (max_count*BLOCK_WIDTH > IMAGE_WIDTH)
+    {
+        BLOCK_WIDTH = static_cast<double>(IMAGE_WIDTH) / static_cast<double>(max_count);
+    }
+    for (size_t i = 0; i < bin_count; i++)
+    {
+        svg_text(TEXT_LEFT, i*(BIN_HEIGHT) + TEXT_BASELINE, bin_namings[i]);
+        for (size_t j = 0; j < (bins[i]); j++)
+        {
+            svg_rect(TEXT_LEFT+TEXT_WIDTH*strlen(bin_namings[i])+(BLOCK_WIDTH*(j-1)), i*(BIN_HEIGHT), BLOCK_WIDTH, BIN_HEIGHT,"red","white");
+        }
+        cout << endl;
+    }
+    svg_end();
+}
+
 int main()
 {
     SetConsoleCP(1251);
@@ -125,6 +206,6 @@ int main()
         }
     }
     const auto bins = make_histogram(numbers, number_count, bin_count);
-    show_histogramm_text(bins,bin_count,bin_namings);
+    show_histogram_svg(bins, bin_count, bin_namings);
     return 0;
 }
