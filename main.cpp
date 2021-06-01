5a73151e43125fd62f0b5015dc1623a2a7ba8817
LvovMD <LvovMD@mpei.ru>
2021-06-01 09:11:22 +0300

    code: Проверка ошибок.

--- main.cpp
+++ main.cpp
@@ -125,6 +125,7 @@ int main(int argc, char* argv[])
             curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, FALSE);
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             res = curl_easy_perform(curl);
+            if (res != 0)cout<<curl_easy_strerror(res);
             curl_easy_cleanup(curl);
         }
         return 0;
