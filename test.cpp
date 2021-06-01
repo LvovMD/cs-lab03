e71ed4d90d777a80b09c9b4b078ad7f7f3aefc93
LvovMD <LvovMD@mpei.ru>
2021-06-01 08:26:49 +0300

    code: Добавлены тесты для индивидуального задания

--- test.cpp
+++ test.cpp
@@ -52,10 +52,32 @@ void test_null()
     assert(max == 0);
 }
 
+void svg_test_positive()
+{
+    double dash = 10;
+    double dasharray = 20;
+    string line = "";
+    line = svg_line(0.0,0.0,10.0,10.0,"black",1.0,dash,dasharray);
+    size_t pos = line.find("stroke-dasharray =)"+to_string(dash)+" "+to_string(dasharray));
+    assert(pos>0);
+}
+
+void svg_test_negative()
+{
+    double dash = 10;
+    double dasharray = 20;
+    string line = "";
+    line = svg_line(0.0,0.0,10.0,10.0,"black",1.0,dash,dasharray);
+    size_t pos = line.find("stroke-dasharray =)"+to_string(dasharray)+" "+to_string(dash));
+    assert(pos=string::npos);
+}
+
 int main()
 {
     test_positive();
     test_negative();
     test_equal();
     test_one();
+    svg_test_positive();
+    svg_test_negative();
 }
